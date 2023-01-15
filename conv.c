450794d94ad317383a7341115d217a5a39f73782
ericklrobles <eglrobles@gmail.com>
2023-01-14 16:53:07 -0800

    test

---
+++ conv.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+int main(void)
+{
+   printf("this is eventually going to be a temp converter sooner or later");
+
+    return 0;
+}
