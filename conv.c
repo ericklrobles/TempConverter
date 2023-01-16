bd6e04f756d0fac1c2e4fec0ab395dc68c4de921
ericklrobles <eglrobles@gmail.com>
2023-01-15 19:12:26 -0800

    erick1

--- conv.c
+++ conv.c
@@ -6,7 +6,40 @@
 
 int main(void)
 {
-   printf("this is eventually going to be a temp converter sooner or later");
+ int weather, celsius, fahrenheit, Kelvin;
+ char input;
+ float input2;
+ char input3;
+ float new_input;
+  
+  printf("Enter the type of temperature measurement that you want to use (f/c/k)\n");
+  scanf(" %c", &input);
+
+  if(input == 'f'){
+    printf("Now enter the degrees in Fahrenheit that you would like to convert to?\n");
+    scanf(" %f", &input2);
+    printf("Almost Forgot! What would you like it to convert to exactly?(c/k)\n");
+    scanf(" %c", &input3);
+        if(input3 == 'c'){
+         new_input = ( (input2 - 32) *.55555555555 );
+          printf(" %f",new_input);   
+        } 
+    }
+     
+
+  
+  
+  if(input == 'k'){
+    printf("You have chosen Kelvin am I right?\n");
+    
+  }
+  
+  if(input == 'c'){
+    printf("You have chosen Celsius am I right?\n");
+  }
+  else{
+       printf("tf you doing my man?!");
+  }
 
     return 0;
 }
